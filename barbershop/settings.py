331af6fb28edc9089356aae389927973c0cc46eb
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2026-03-29 14:51:35 +0800

    Last Sync: 2026-03-29 14:51 (Mobile)

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -26,6 +26,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 # ── APPS ─────────────────────────────────────────────────────
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
